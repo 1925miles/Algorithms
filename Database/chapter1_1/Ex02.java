308ea743c24dd75845564410efe974a5f7f936a2
S6-Miles <84891609+S6-Miles@users.noreply.github.com>
2021-07-22 17:35:02 +0800

    Update Ex02.java

--- Database/chapter1_1/Ex02.java
+++ Database/chapter1_1/Ex02.java
@@ -1,10 +1,10 @@
-package com.jimmysun.algorithms.chapter1_1;
+package Algorithms;
 
-public class Ex02 {
-	public static void main(String[] args) {
-		System.out.println((1 + 2.236) / 2);
-		System.out.println(1 + 2 + 3 + 4.0);
-		System.out.println(4.1 >= 4);
-		System.out.println(1 + 2 + "3");
-	}
+public class lx_2 {
+    public static void main(String[] args) {
+        System.out.println((1 + 2.236) / 2);//a【1.618】表达式即为3.236/2，是浮点数的除运算（与1.1.1.a做区别）
+        System.out.println(1 + 2 + 3 + 4.0);//b【10.0】数据类型转换，数值自动提升为高级的数据类型1,2,3变为1.0,2.0,3.0
+        System.out.println(4.1 >= 4);//c【true】
+        System.out.println(1 + 2 + "3");//d【33】输出为两个3，但两个3并不相同，前者表示由1+2得整数3，后者表示为字符3
+    }
 }
