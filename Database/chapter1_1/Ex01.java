781de7c7807a699e2e5f77e8e823eb51e83f6114
S6-Miles <84891609+S6-Miles@users.noreply.github.com>
2021-07-22 17:53:45 +0800

    Update Ex01.java

--- Database/chapter1_1/Ex01.java
+++ Database/chapter1_1/Ex01.java
@@ -1,9 +1,9 @@
-package com.jimmysun.algorithms.chapter1_1;
+package Algorithms;
 
-public class Ex01 {
-	public static void main(String[] args) {
-		System.out.println((0 + 15) / 2);
-		System.out.println(2.0e-6 * 100000000.1);
-		System.out.println(true && false || true && true);
-	}
+public class lx_1 {
+    public static void main(String[] args) {
+        System.out.println((0 + 15) / 2);//a【7】表达式即为15/2，是整型量的除运算
+        System.out.println(2.0e-6 * 100000000.1);//b【200.0000002】2.0e-6为浮点数的科学计数法，等同于0.000002
+        System.out.println(true && false || true && true);//c【true】按优先级运算，逻辑运算符中，！拥有最高优先级，其次是&&，之后是||，然后为&、|、^
+    }
 }
